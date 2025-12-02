82c0ccd4317af3408b4ee92e008b60813a49e4cd
Ronaldo de Arajo Ribeiro Filho-RONALDO <ronaldo@motorola.com>
2025-12-02 17:10:04 -0300

    feat: updates from the course

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,16 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { MatInputModule } from '@angular/material/input';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   imports: [
     PostCreateComponent, MatInputModule,
     HeaderComponent, PostListComponent,
-  ],
+    RouterOutlet
+],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
