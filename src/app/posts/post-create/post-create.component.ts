91b8efaf50695492d4635cde6ee147af80dbcb27
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-12-03 14:12:48 -0300

    feat: new features on post-create

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { PostsService } from '../post.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
+import { P } from '@angular/cdk/keycodes';
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-post-create',
@@ -14,21 +18,53 @@ import { PostsService } from '../post.service';
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
-  ],
+    MatProgressSpinner
+],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css',
 })
-export class PostCreateComponent {
-  enteredTitle = '';
-  enteredContent = '';
+export class PostCreateComponent implements OnInit{
+  enteredTitle = "";
+  enteredContent = "";
+  post: Post | undefined;
+  isLoading = false;
+  private mode = 'create';
+  private postId: string | null = null;
 
-  postsService = inject(PostsService);
 
-  onAddPost(form: NgForm) {
+  constructor(public postsService: PostsService, public route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.isLoading = true;
+        this.postsService.getPost(this.postId!).subscribe(postData => {
+          this.isLoading = false;
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
+        });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    this.isLoading = true;
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId!,
+        form.value.title,
+        form.value.content
+      );
+    }
     form.resetForm();
   }
 }***REMOVED***
