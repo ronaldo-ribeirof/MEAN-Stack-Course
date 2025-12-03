bc6c365931256cdf754c59d2f5839679179ef93a
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-12-03 14:11:55 -0300

    feat: enhanced backend

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const postRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://ron:tHlM3unh9hdUB5YV@cluster0.cyai759.mongodb.net/node-angular?retryWrites=true")
+.then(() => {
+  console.log('Connected to database!');
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postRoutes);
+
+module.exports = app;***REMOVED***
+***REMOVED***
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs'
 import {} from 'rxjs/operators';
 import { map } from 'rxjs/operators'
 import { Post } from './post.model';
+import { Router } from '@angular/router';
 
 @Injectable({providedIn: 'root'})
 
@@ -11,7 +12,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http
@@ -37,6 +38,12 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{ _id: string, title: string, content: string }>(
+      "http://localhost:3000/api/posts/" + id
+    );
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: '', title: title, content: content };
     this.http
@@ -45,7 +52,21 @@ export class PostsService {
         const id = responseData.postId;
         post.id = id;
         this.posts.push(post)
-        this.postsUpdated.next([...this.posts]);
+        this.postsUpdated.next([...this.posts])
+        this.router.navigate(["/"])
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
+    this.http.put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts])
+        this.router.navigate(["/"])
       });
   }
 
