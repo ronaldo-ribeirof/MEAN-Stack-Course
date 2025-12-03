32bce9a7904cbea42e2ebd9794a8565cd7164a89
Ronaldo de Arajo Ribeiro Filho-RONALDO <ronaldo@motorola.com>
2025-12-03 14:13:38 -0300

    feat: now header have navigation and new style

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterLink } from "@angular/router";
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, RouterLink, MatAnchor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
