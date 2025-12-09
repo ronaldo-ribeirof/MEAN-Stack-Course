1b1bc1b9b82418e1d8bb5f3c01013228a3e00bbb
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-12-09 10:55:53 -0300

    feat: new basic login resources

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent },
   { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'login', component: LoginComponent },
 ];***REMOVED***
 ***REMOVED***
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatCard } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, MatInputModule, MatProgressSpinner, MatCard, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {}***REMOVED***
+***REMOVED***
