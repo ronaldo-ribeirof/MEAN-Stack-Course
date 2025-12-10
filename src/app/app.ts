588999dd0d3e5e0c2e0a9c4843347f02396898c5
Ronaldo Ribeiro <filhoronaldo.ribeiro@gmail.com>
2025-12-10 15:52:03 -0300

    git commit -m "feat: now the tokens are saved
    Co-authored-by: Ronaldo de Arajo Ribeiro Filho-RONALDO <ronaldo@motorola.com>"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('mean-course');
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.autoAuthUser();
+  }
 }
